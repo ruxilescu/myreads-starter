77b8d7747c94f1e451a4c75b7c597a89603d4cb3
Rodrick Bloomfield <23622438+bloom305@users.noreply.github.com>
2018-09-09 23:08:49 -0400

    Convert to components

---
+++ src/components/Book.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+export default class Book extends Component {
+  render() {
+
+    const { book } = this.props;
+    const { imageLinks, authors, title, shelf } = book;
+
+    return (
+      <li>
+        <div className="book">
+          <div className="book-top">
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${imageLinks.thumbnail})` }}></div>
+            <div className="book-shelf-changer">
+              <select value={shelf} onChange={() => {}}>
+                <option value="move" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
+          </div>
+          <div className="book-title">{title}</div>
+          {authors.map(author => (<div key={author} className="book-authors">{author}</div>))}
+        </div>
+      </li>
+    )
+  }
+}
